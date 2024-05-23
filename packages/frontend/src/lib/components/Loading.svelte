d718adb550d1fe4fe32f665cd953f5d7126d6a19
gyra0229 <loginerrore04@gmail.com>
2024-05-24 03:13:46 +0800

    feat: Add Loading component for displaying loading state
    This commit adds a new component called Loading, which displays a loading spinner while content is being loaded. The component is implemented in the `Loading.svelte` file and uses a GIF image for the spinner animation. This component will be used to improve the user experience by providing visual feedback during loading operations.
    
    Note: This commit message follows the conventional commit format and provides a summary of the changes made.

---
+++ packages/frontend/src/lib/components/Loading.svelte
@@ -0,0 +1,9 @@
+<script>
+  import loadingGif from "$lib/images/loading.gif";
+</script>
+
+<div
+  class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+>
+  <img src={loadingGif} alt="Loading..." class="w-24 h-24" />
+</div>
